652ceb69157a39b78ed2c0864ea38d46c3a4d04c
alphaplato <plato.sg.lee@gmail.com>
2019-03-09 11:00:55 +0800

    add Fibonacci

---
+++ DynamicProgram/Fibonacci.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int Fibonacci(int n){
+    if(n==0) return 0;
+    if(n==1) return 1;
+    int dp[n]; //这里从0开始为第一个数
+    dp[0] = 0;
+    dp[1] = 1;
+    for(int i=2; i<n; i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    return dp[n-1];
+}
+
+int main(){
+    cout << "Fibonacci nth:" << Fibonacci(n) << endl;
+    return 0;
+}
+

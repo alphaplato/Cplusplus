4b8ee216bba5d28ad793eb72f7390c6f4037ce8b
alphaplato <plato.sg.lee@gmail.com>
2019-04-23 23:51:33 +0800

    reverse list.cc bug,can't delete last one data

--- DataStructure/list.cc
+++ DataStructure/list.cc
@@ -8,6 +8,20 @@ struct IntLtnode {
     IntLtnode* pnext;
 };
 
+void display(IntLtnode* head){ //链表的输出
+    if (head == nullptr) {
+        std::cout << "No data!" << std::endl;
+        return;
+    }
+    IntLtnode *plt;
+    plt = head;
+    while(plt){
+        std::cout << plt->node << " ";
+        plt = plt->pnext;
+    }
+    std::cout << std::endl;
+}
+
 void Init(int* a, int size, IntLtnode* &head) { //链表的初始化
     if (size == 0) {
         std::cout << "No data initlize!" << std::endl;
@@ -22,20 +36,6 @@ void Init(int* a, int size, IntLtnode* &head) { //链表的初始化
         pos->pnext = temp;
         pos = pos->pnext;
     }   
-}
-
-void display(IntLtnode* head){ //链表的输出
-    if (head == nullptr) {
-        std::cout << "No data!" << std::endl;
-        return;
-    }
-    IntLtnode *plt;
-    plt = head;
-    while(plt){
-        std::cout << plt->node << " ";
-        plt = plt->pnext;
-    }
-    std::cout << std::endl;
 }
 
 void reverse(IntLtnode* &head) { //链表的颠倒逆向
@@ -54,14 +54,41 @@ void reverse(IntLtnode* &head) { //链表的颠倒逆向
     head = preptr;
 }
 
+void delnode(int a, IntLtnode* &head) {
+    IntLtnode* pl = head;
+    if (pl == nullptr) {
+        std::cout << "No data:" << a << "." << std::endl;
+        return;
+    }
+    while(pl->pnext){
+        if (pl->pnext->node == a) {
+            pl->pnext = pl->pnext->pnext;
+            delete pl->pnext;
+        } else {
+            pl = pl->pnext;
+        }
+    }
+    if (head->node == a) {
+        IntLtnode* temp;
+        temp = head;
+        head = head->pnext;
+        delete temp;
+    }
+}
+
+
 int main(){
     int a[] = {1,2,3,4,5};
     IntLtnode* intlt;
-    Init(a, 5, intlt);
     std::cout << "initlize the list:";
+    Init(a, 5, intlt);
     display(intlt);
 
-    reverse(intlt);
     std::cout << "reverse the list:";
+    reverse(intlt);
+    display(intlt);
+
+    std::cout << "delete the data in list:";
+    delnode(1,intlt);
     display(intlt);
 }

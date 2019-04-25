e7e15c94671f0c26e6bc1f1c777dfd76b8b0ef1e
alphaplato <plato.sg.lee@gmail.com>
2019-04-25 09:50:13 +0800

    easy the mo iter pretravel

--- DataStructure/bitree.cc
+++ DataStructure/bitree.cc
@@ -2,52 +2,69 @@
 #include <queue>
 #include <stack>
 
-struct TreeNode{
-    TreeNode(int key):key(key),left(nullptr),right(nullptr){}
-    TreeNode* left;
-    TreeNode* right;
+struct TreeNode
+{
+    TreeNode(int key) : key(key), left(nullptr), right(nullptr) {}
+    TreeNode *left;
+    TreeNode *right;
     int key;
 };
 
-void CreateBST(int a,TreeNode* &t){
-    if(t){
-        if(t->key > a){
-            CreateBST(a,t->left);
-        }else{
-            CreateBST(a,t->right);
+void CreateBST(int a, TreeNode *&t)
+{
+    if (t)
+    {
+        if (t->key > a)
+        {
+            CreateBST(a, t->left);
         }
-    }else{
+        else
+        {
+            CreateBST(a, t->right);
+        }
+    }
+    else
+    {
         t = new TreeNode(a);
     }
 }
 
-int MidTravel(TreeNode* &t){
-    if(t){
-        MidTravel(t->left);
+int midTravel(TreeNode *&t)
+{
+    if (t)
+    {
+        midTravel(t->left);
         std::cout << t->key << " ";
-        MidTravel(t->right);
-    }else{
+        midTravel(t->right);
+    }
+    else
+    {
         return 0;
     }
     return 0;
 }
 
-
-int WidthTravel(TreeNode* &t){
-    std::queue<TreeNode*> nodes;
-    if(!t){
+int widthTravel(TreeNode *&t)
+{
+    std::queue<TreeNode *> nodes;
+    if (!t)
+    {
         return 1;
     }
-    if(nodes.empty()){
+    if (nodes.empty())
+    {
         nodes.push(t);
     }
-    while(!nodes.empty()){
-        TreeNode* p = nodes.front();
+    while (!nodes.empty())
+    {
+        TreeNode *p = nodes.front();
         std::cout << p->key << " ";
-        if(p->left){
+        if (p->left)
+        {
             nodes.push(p->left);
         }
-        if(p->right){
+        if (p->right)
+        {
             nodes.push(p->right);
         }
         nodes.pop();
@@ -55,72 +72,76 @@ int WidthTravel(TreeNode* &t){
     return 0;
 }
 
-int PreTravel(TreeNode* t){
-    if(t){
+int preTravel(TreeNode *t)
+{
+    if (t)
+    {
         std::cout << t->key << " ";
-        PreTravel(t->left);
-        PreTravel(t->right);
+        preTravel(t->left);
+        preTravel(t->right);
     }
     return 0;
 };
 
-int PreTravel2(TreeNode* t){
-    std::stack<TreeNode*> s;
-    if(t){
+int preTravel2(TreeNode *t)
+{
+    std::stack<TreeNode *> s;
+    if (t)
+    {
         s.push(t);
-        std::cout << t->key << " ";
     }
-    TreeNode* p = t;
-    while(1){
-        if(p->left){
-            p = p->left;
-            std::cout << p->key << " ";
-            s.push(p);
-        }else{
-            if(p->right){
-                s.pop();
-                p = p->right;
-                std::cout << p->key << " ";
-                s.push(p);
-            }else{
-                s.pop();
-                if(s.empty()){
-                    break;
-                }else{
-                    p = (s.top())->right;
-                    std::cout << p->key << " ";
-                    s.pop();
-                }
-            }
+    while (!s.empty())
+    {
+        TreeNode *p = s.top();
+        std::cout << p->key << " ";
+        s.pop();
+        if (p->right)
+        {
+            s.push(p->right);
+        }
+        if (p->left)
+        {
+            s.push(p->left);
         }
     }
     return 0;
 }
 
-
-int ListTree(TreeNode* &head){
-    if(!head){
+int listTree(TreeNode *&head)
+{
+    if (!head)
+    {
         return 0;
     }
-    if(head->left){
-        TreeNode* node = head->left;
+    if (head->left)
+    {
+        TreeNode *node = head->left;
         head->left = node->right;
         node->right = head;
         head = node;
-        ListTree(head);    
-    }else if(head->right){
-        ListTree(head->right);
+        listTree(head);
+    }
+    else if (head->right)
+    {
+        listTree(head->right);
     }
     return 0;
 }
 
-int FindTreeNode(int a,TreeNode* &head, TreeNode* &node){
-    if(head){
-        if(head->key > a){
-            FindTreeNode(a, head->left, node);
-        }else if(head->key < a){
-            FindTreeNode(a, head->right, node);
-        }else{
+int findTreeNode(int a, TreeNode *&head, TreeNode *&node)
+{
+    if (head)
+    {
+        if (head->key > a)
+        {
+            findTreeNode(a, head->left, node);
+        }
+        else if (head->key < a)
+        {
+            findTreeNode(a, head->right, node);
+        }
+        else
+        {
             node = head;
             return 1;
         }
@@ -128,17 +149,22 @@ int FindTreeNode(int a,TreeNode* &head, TreeNode* &node){
     return 0;
 }
 
-int DelTreeNode(TreeNode* t){
-    if(t->left == NULL && t->right == NULL){
+int delTreeNode(TreeNode *t)
+{
+    if (t->left == NULL && t->right == NULL)
+    {
         *t = NULL;
-    }else if(t->left){
+    }
+    else if (t->left)
+    {
         *t = *(t->left);
-    }else if(t->right){
+    }
+    else if (t->right)
+    {
         *t = *(t->right);
     }
     return 0;
 }
-
 
 bool isAVL(TreeNode *pRoot, int &height)
 {
@@ -148,14 +174,14 @@ bool isAVL(TreeNode *pRoot, int &height)
         return true;
     }
     // 判断左子树是否是平衡树
-    int  heightLeft;
+    int heightLeft;
     bool resultLeft = isAVL(pRoot->left, heightLeft);
- 
+
     // 判断右子树是否是平衡树
     int heightRight;
     bool resultRight = isAVL(pRoot->right, heightRight);
     // 左右子树都为AVL，判断根节点的平衡性
-    if (resultLeft && resultRight &&abs(heightLeft - heightRight) < 2)
+    if (resultLeft && resultRight && abs(heightLeft - heightRight) < 2)
     {
         height = (heightLeft > heightRight) ? (heightLeft + 1) : (heightRight + 1);
         return true;
@@ -163,31 +189,39 @@ bool isAVL(TreeNode *pRoot, int &height)
     return false;
 }
 
-int main(){
-    int a[7] = {6,3,5,2,4,9,7};
-    TreeNode* head = NULL;
-    for(int i=0;i < 7;i++){
-        CreateBST(a[i],head);
+int main()
+{
+    int a[7] = {6, 3, 5, 2, 4, 9, 7};
+    TreeNode *head = NULL;
+    for (int i = 0; i < 7; i++)
+    {
+        CreateBST(a[i], head);
     }
     std::cout << "中序遍历：";
-    MidTravel(head);
-    std::cout << std::endl << "广度优先遍历";
-    WidthTravel(head);
-    std::cout << std::endl << "前序遍历，递归:";
-    PreTravel(head);
-    std::cout << std::endl << "前序遍历，非递归:";
-    PreTravel2(head);
-    std::cout << std::endl << "将树变成一条直链:";
-    ListTree(head);
-    PreTravel(head);
-    TreeNode* des;
-    std::cout << std::endl << "查找" << 7 << ":";
-    std::cout << std::endl << "是否查找到，1 或 0：" 
-        << FindTreeNode(7,head,des) << std::endl;
+    midTravel(head);
+    std::cout << std::endl
+              << "广度优先遍历";
+    widthTravel(head);
+    std::cout << std::endl
+              << "前序遍历，递归:";
+    preTravel(head);
+    std::cout << std::endl
+              << "前序遍历，非递归:";
+    preTravel2(head);
+    std::cout << std::endl
+              << "将树变成一条直链:";
+    listTree(head);
+    preTravel(head);
+    TreeNode *des;
+    std::cout << std::endl
+              << "查找" << 7 << ":";
+    std::cout << std::endl
+              << "是否查找到，1 或 0："
+              << findTreeNode(7, head, des) << std::endl;
     std::cout << "删除" << 7 << ":";
-    DelTreeNode(des);
+    delTreeNode(des);
     std::cout << "mid travel:";
-    MidTravel(head);
+    midTravel(head);
     std::cout << std::endl;
     return 0;
 }
